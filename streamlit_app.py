3428c4a7f5874e8a7e30218b7445db2cb93db5a1
Lala Arnav Vatsal <lalaarnav003@gmail.com>
2023-08-04 01:02:39 +0530

    Initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,295 @@
+import nltk
+import spacy
+
+nltk.download('stopwords')
+spacy.load('en_core_web_sm')
+
+import streamlit as st
+
+import pandas as pd
+import base64
+import random
+import time
+import datetime
+import io
+
+from pyresparser import ResumeParser
+
+from pdfminer3.layout import LAParams, LTTextBox
+from pdfminer3.pdfpage import PDFPage
+from pdfminer3.pdfinterp import PDFResourceManager
+from pdfminer3.pdfinterp import PDFPageInterpreter
+from pdfminer3.converter import TextConverter
+
+import pymysql
+from PIL import Image
+
+
+def get_table_download_link(df, filename, text):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    # href = f'<a href="data:file/csv;base64,{b64}">Download Report</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">{text}</a>'
+    return href
+
+
+def pdf_reader(file):
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+    with open(file, 'rb') as fh:
+        for page in PDFPage.get_pages(fh,
+                                      caching=True,
+                                      check_extractable=True):
+            page_interpreter.process_page(page)
+            print(page)
+        text = fake_file_handle.getvalue()
+
+    # close open handles
+    converter.close()
+    fake_file_handle.close()
+    return text
+
+
+def show_pdf(file_path):
+    with open(file_path, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    with st.expander("Show CV"):
+        st.markdown(pdf_display, unsafe_allow_html=True)
+
+
+connection = pymysql.connect(host='localhost', user='root', password='12345678')
+cursor = connection.cursor()
+
+
+def insert_data(name, email, res_score, timestamp, no_of_pages, reco_field, skills):
+    DB_table_name = 'user_data'
+    insert_sql = "insert into " + DB_table_name + """
+    values (0,%s,%s,%s,%s,%s,%s,%s)"""
+    rec_values = (
+        name, email, str(res_score), timestamp, str(no_of_pages), reco_field, skills)
+    cursor.execute(insert_sql, rec_values)
+    connection.commit()
+
+
+st.set_page_config(
+    page_title="Human Resource Management Portal",
+    page_icon='./Resources/Logo.png',
+)
+
+
+def run():
+    st.title("Human Resource Management")
+    st.sidebar.markdown("# Choose User")
+    activities = ["User", "Admin"]
+    choice = st.sidebar.selectbox("Choose among the given options:", activities)
+
+    img = Image.open('Resources/Logo.png')
+    img = img.resize((250, 250))
+    st.image(img)
+
+    # Create the DB
+    db_sql = """CREATE DATABASE IF NOT EXISTS MY_DATABASE;"""
+    cursor.execute(db_sql)
+    connection.select_db("MY_DATABASE")
+
+    # Create table
+    DB_table_name = 'user_data'
+    table_sql = "CREATE TABLE IF NOT EXISTS " + DB_table_name + """
+                    (ID INT NOT NULL AUTO_INCREMENT,
+                     Name varchar(100) NOT NULL,
+                     Email_ID VARCHAR(50) NOT NULL,
+                     resume_score VARCHAR(8) NOT NULL,
+                     Timestamp VARCHAR(50) NOT NULL,
+                     Page_no VARCHAR(5) NOT NULL,
+                     Predicted_Field VARCHAR(25) NOT NULL,
+                     User_level VARCHAR(30) NOT NULL,
+                     Actual_skills VARCHAR(300) NOT NULL,
+                     Recommended_skills VARCHAR(300) NOT NULL,
+                     Recommended_courses VARCHAR(600) NOT NULL,
+                     PRIMARY KEY (ID));
+                    """
+    cursor.execute(table_sql)
+    if choice == 'User':
+        pdf_file = st.file_uploader("Choose your Resume", type=["pdf"])
+        if pdf_file is not None:
+            save_image_path = './Uploaded Resumes/' + pdf_file.name
+            with open(save_image_path, "wb") as f:
+                f.write(pdf_file.getbuffer())
+            show_pdf(save_image_path)
+            resume_data = ResumeParser(save_image_path).get_extracted_data()
+            if resume_data:
+                resume_text = pdf_reader(save_image_path)
+
+                st.header("Hello " + resume_data['name'])
+                st.subheader("**Your Basic info**")
+                try:
+                    st.text('Name: ' + resume_data['name'])
+                    st.text('Email: ' + resume_data['email'])
+                    st.text('Contact: ' + resume_data['mobile_number'])
+                except:
+                    pass
+
+                st.subheader("**Skills Detected**")
+                st.markdown(resume_data['skills'])
+
+                ##  recommendation
+                ds_keyword = ['tensorflow', 'keras', 'pytorch', 'machine learning', 'deep Learning', 'flask',
+                              'streamlit']
+                web_keyword = ['react', 'django', 'node jS', 'react js', 'php', 'laravel', 'magento', 'wordpress',
+                               'javascript', 'angular js', 'c#', 'flask']
+                android_keyword = ['android', 'android development', 'flutter', 'kotlin', 'xml', 'kivy']
+                ios_keyword = ['ios', 'ios development', 'swift', 'cocoa', 'cocoa touch', 'xcode']
+                uiux_keyword = ['ux', 'adobe xd', 'figma', 'zeplin', 'balsamiq', 'ui', 'prototyping', 'wireframes',
+                                'storyframes', 'adobe photoshop', 'photoshop', 'editing', 'adobe illustrator',
+                                'illustrator', 'adobe after effects', 'after effects', 'adobe premier pro',
+                                'premier pro', 'adobe indesign', 'indesign', 'wireframe', 'solid', 'grasp',
+                                'user research', 'user experience']
+
+                reco_field = ''
+
+                for i in resume_data['skills']:
+                    ## Data science
+                    if i.lower() in ds_keyword:
+                        print(i.lower())
+                        reco_field = 'Data Science'
+                        st.success("** Our analysis says you are looking for Data Science Jobs.**")
+                        break
+
+                    ## Web development
+                    elif i.lower() in web_keyword:
+                        print(i.lower())
+                        reco_field = 'Web Development'
+                        st.success("** Our analysis says you are looking for Web Development Jobs **")
+                        break
+
+                    ## Android App Development
+                    elif i.lower() in android_keyword:
+                        print(i.lower())
+                        reco_field = 'Android Development'
+                        st.success("** Our analysis says you are looking for Android App Development Jobs **")
+                        break
+
+                    ## IOS App Development
+                    elif i.lower() in ios_keyword:
+                        print(i.lower())
+                        reco_field = 'IOS Development'
+                        st.success("** Our analysis says you are looking for IOS App Development Jobs **")
+                        break
+
+                    ## Ui-UX Recommendation
+                    elif i.lower() in uiux_keyword:
+                        print(i.lower())
+                        reco_field = 'UI-UX Development'
+                        st.success("** Our analysis says you are looking for UI-UX Development Jobs **")
+                        break
+
+                ## Insert into table
+                ts = time.time()
+                cur_date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
+                cur_time = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+                timestamp = str(cur_date + '_' + cur_time)
+
+                ### Resume recommendation
+                st.subheader("**Resume Recommendations**")
+                resume_score = 0
+                if 'Objective' in resume_text:
+                    resume_score = resume_score + 20
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #1ed760;'>[+] Awesome! You have added Objective</h4>''',
+                        unsafe_allow_html=True)
+                else:
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #fabc10;'>[-] According to our recommendation please add your career objective, it will give your career intension to the Recruiters.</h4>''',
+                        unsafe_allow_html=True)
+
+                if 'Declaration' in resume_text:
+                    resume_score = resume_score + 20
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #1ed760;'>[+] Awesome! You have added Delcaration✍/h4>''',
+                        unsafe_allow_html=True)
+                else:
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #fabc10;'>[-] According to our recommendation please add Declaration✍. It will give the assurance that everything written on your resume is true and fully acknowledged by you</h4>''',
+                        unsafe_allow_html=True)
+
+                if 'Hobbies' or 'Interests' in resume_text:
+                    resume_score = resume_score + 20
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #1ed760;'>[+] Awesome! You have added your Hobbies⚽</h4>''',
+                        unsafe_allow_html=True)
+                else:
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #fabc10;'>[-] According to our recommendation please add Hobbies⚽. It will show your persnality to the Recruiters and give the assurance that you are fit for this role or not.</h4>''',
+                        unsafe_allow_html=True)
+
+                if 'Achievements' in resume_text:
+                    resume_score = resume_score + 20
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #1ed760;'>[+] Awesome! You have added your Achievements🏅 </h4>''',
+                        unsafe_allow_html=True)
+                else:
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #fabc10;'>[-] According to our recommendation please add Achievements🏅. It will show that you are capable for the required position.</h4>''',
+                        unsafe_allow_html=True)
+
+                if 'Projects' in resume_text:
+                    resume_score = resume_score + 20
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #1ed760;'>[+] Awesome! You have added your Projects👨‍💻 </h4>''',
+                        unsafe_allow_html=True)
+                else:
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #fabc10;'>[-] According to our recommendation please add Projects👨‍💻. It will show that you have done work related the required position or not.</h4>''',
+                        unsafe_allow_html=True)
+
+                st.subheader("**Resume Score**")
+                st.markdown(
+                    """
+                    <style>
+                        .stProgress > div > div > div > div {
+                            background-color: #d73b5c;
+                        }
+                    </style>""",
+                    unsafe_allow_html=True,
+                )
+                my_bar = st.progress(0)
+                score = 0
+                for percent_complete in range(resume_score):
+                    score += 1
+                    time.sleep(0.1)
+                    my_bar.progress(percent_complete + 1)
+                st.success('** Your Resume Writing Score: ' + str(score) + '**')
+                st.warning(
+                    "** Note: This score is calculated based on the content that you have added in your Resume. **")
+
+                insert_data(resume_data['name'], resume_data['email'], str(resume_score), timestamp,
+                            str(resume_data['no_of_pages']), reco_field, str(resume_data['skills'])
+                            )
+
+                connection.commit()
+            else:
+                st.error('Something went wrong..')
+    else:
+        ## Admin Side
+        st.success('Welcome to Admin Portal')
+
+        ad_user = st.text_input("Username")
+        ad_password = st.text_input("Password", type='password')
+        if st.button('Login'):
+            if ad_user == 'admin' and ad_password == 'admin123':
+                st.success("Welcome Admin")
+                cursor.execute('''SELECT * FROM user_data''')
+                data = cursor.fetchall()
+                st.header("User Data")
+                df = pd.DataFrame(data, columns=['ID', 'Name', 'Email', 'Resume Score', 'Timestamp', 'Total Page',
+                                                 'Predicted Field', 'Skills'])
+                st.dataframe(df)
+
+            else:
+                st.error("Wrong ID & Password Provided")
+
+
+run()
